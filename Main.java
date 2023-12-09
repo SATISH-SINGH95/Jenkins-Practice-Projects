44169624c7586a25230f5594c1629b7d1c1f029f
satish <sk95satish@gmail.com>
2023-12-09 18:25:48 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class Main{
+
+    public static void main(String[] args) {
+        System.out.println("This is first line");
+        System.out.println("This is second line");
+        System.out.println("Today's date is "+ new Date());
+    }
+
+}
